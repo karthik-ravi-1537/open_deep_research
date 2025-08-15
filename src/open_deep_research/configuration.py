4649f4c39c0b259fb7292e7dcaeb9598d436dcc0
Karthik Ravi <karthik.ravi.1537@gmail.com>
2025-08-15 15:22:02 -0700

    Config Mods + Minor Fixes.

--- src/open_deep_research/configuration.py
+++ src/open_deep_research/configuration.py
@@ -119,11 +119,13 @@ class Configuration(BaseModel):
     )
     # Model Configuration
     summarization_model: str = Field(
-        default="openai:gpt-4.1-mini",
+        # default="openai:gpt-4.1-mini",
+        default="google:gemini-2.5-flash",
         metadata={
             "x_oap_ui_config": {
                 "type": "text",
-                "default": "openai:gpt-4.1-mini",
+                # "default": "openai:gpt-4.1-mini",
+                "default": "google:gemini-2.5-flash",
                 "description": "Model for summarizing research results from Tavily search results"
             }
         }
@@ -151,11 +153,13 @@ class Configuration(BaseModel):
         }
     )
     research_model: str = Field(
-        default="openai:gpt-4.1",
+        # default="openai:gpt-4.1",
+        default="google:gemini-2.5-pro",
         metadata={
             "x_oap_ui_config": {
                 "type": "text",
-                "default": "openai:gpt-4.1",
+                # "default": "openai:gpt-4.1",
+                "default": "google:gemini-2.5-pro",
                 "description": "Model for conducting research. NOTE: Make sure your Researcher Model supports the selected search API."
             }
         }
@@ -171,11 +175,13 @@ class Configuration(BaseModel):
         }
     )
     compression_model: str = Field(
-        default="openai:gpt-4.1",
+        # default="openai:gpt-4.1",
+        default="google:gemini-2.5-pro",
         metadata={
             "x_oap_ui_config": {
                 "type": "text",
-                "default": "openai:gpt-4.1",
+                # "default": "openai:gpt-4.1",
+                "default": "google:gemini-2.5-pro",
                 "description": "Model for compressing research findings from sub-agents. NOTE: Make sure your Compression Model supports the selected search API."
             }
         }
@@ -191,11 +197,13 @@ class Configuration(BaseModel):
         }
     )
     final_report_model: str = Field(
-        default="openai:gpt-4.1",
+        # default="openai:gpt-4.1",
+        default="google:gemini-2.5-pro",
         metadata={
             "x_oap_ui_config": {
                 "type": "text",
-                "default": "openai:gpt-4.1",
+                # "default": "openai:gpt-4.1",
+                "default": "google:gemini-2.5-pro",
                 "description": "Model for writing the final report from all research findings"
             }
         }
